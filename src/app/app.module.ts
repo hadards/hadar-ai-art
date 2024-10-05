5d67b9d573f8e9f5b6e747dfb92f7e1541c66952
Hadar <hadards@yahoo.com>
2024-10-05 10:49:59 +0300

    mobile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './start-page/start-page.component';
+import { StartPageMobileComponent } from './start-page-mobile/start-page-mobile.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
-    StartPageComponent
+    StartPageComponent,
+    StartPageMobileComponent
    ],
   imports: [
     BrowserModule,
