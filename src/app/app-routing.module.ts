a475c50330b1967ba17a1326b3d7f82a3e4b133f
Hadar <hadards@yahoo.com>
2024-10-06 08:27:03 +0300

    mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
+import { StartPageMobileComponent } from './start-page-mobile/start-page-mobile.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component:  window.screen.width > 767 ? StartPageComponent : StartPageMobileComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
