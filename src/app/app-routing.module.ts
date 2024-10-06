d56bb5e1b95224febf2e9c4562a6ccee1ac7cf3d
Hadar <hadards@yahoo.com>
2024-10-06 09:32:03 +0300

    try fix route deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { StartPageComponent } from './start-page/start-page.component';
 import { StartPageMobileComponent } from './start-page-mobile/start-page-mobile.component';
 
-const routes: Routes = [{
-  path: '',
-  component:  window.screen.width > 767 ? StartPageComponent : StartPageMobileComponent
-}];
+const routes: Routes = [
+  {path: 'hadar-ai-art',  component:  window.screen.width > 767 ? StartPageComponent : StartPageMobileComponent},
+  {path: '',  component:  window.screen.width > 767 ? StartPageComponent : StartPageMobileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
